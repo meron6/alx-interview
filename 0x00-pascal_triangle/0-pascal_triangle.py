e2c1690f7eda7553fb783e697453a7535cee0044
meron6 <khozameron5@gmail.com>
2024-07-25 14:33:12 -0700

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,44 +1,62 @@
-#!/usr/bin/env python3
-"""Calculate Pascal's Triangle"""
-
-def get_triangle(num):
-    """Return 2D list of Pascal's Triangle up to num rows"""
-    if num <= 0:
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+    :param n: Number of rows in Pascal's Triangle.
+    :return: A list of lists representing Pascal's Triangle.
+    """
+    if n <= 0:
         return []
-    
-    ret = [[1]]
-    for i in range(1, num):
-        row = [1]  # Start with 1
+
+    triangle = [[1]]  # Initialize the first row
+
+    for i in range(1, n):
+        row = [1]  # Start each row with 1
+        # Calculate the intermediate values
         for j in range(1, i):
-            row.append(ret[i-1][j-1] + ret[i-1][j])
-        row.append(1)  # End with 1
-        ret.append(row)
-    
-    return ret
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # End each row with 1
+        triangle.append(row)
+
+    return triangle
+
+
+def print_left(triangle):
+    """
+    Prints the triangle in a left-aligned format to demonstrate data structure
+    """
+    for row in triangle:
+        print(" ".join(map(str, row)))
+
+
+def print_centre(triangle):
+    """
+    Prints the triangle in a conventional centred format
+    """
+    max_width = len(" ".join(map(str, triangle[-1])))
+    for row in triangle:
+        row_str = " ".join(map(str, row))
+        print(row_str.center(max_width))
+
 
 def main():
-    """Get number of rows to calculate from the user and show a pretty triangle"""
-    try:
-        num = int(input('Number of rows: '))
-    except ValueError:
-        print('Please input a valid integer.')
-        exit(1)
-    
-    triangle = get_triangle(num)
-    
-    if not triangle:
-        print('Number of rows must be a positive integer.')
-        return
-    
-    length = len(triangle)
-    
-    if length > 100:
-        print('Too large, not displaying.')
-        return
-    
-    for i in range(length):
-        row = triangle[i]
-        print(' ' * (length - i - 1) + ' '.join(str(j) for j in row))
+    """
+    Create and populate Pascal's Triangle
+    and then print it in two formats
+    """
+    print("---------------------")
+    print("| Pascal's Triangle |")
+    print("---------------------\n")
 
-if __name__ == '__main__':
+    n = 8  # Number of rows
+    triangle = pascal_triangle(n)
+
+    print("Left-aligned Pascal's Triangle:")
+    print_left(triangle)
+
+    print("\nCentred Pascal's Triangle:")
+    print_centre(triangle)
+
+
+if __name__ == "__main__":
     main()
+

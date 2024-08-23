ff8be8464e3a87012fadb1acef14a9fee8254cce
meron6 <khozameron5@gmail.com>
2024-08-22 20:47:26 -0700

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,31 +1,26 @@
+#!/usr/bin/python3
+
+
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    """Determines if given data represents valid UTF-8 encoding
 
-    # Masks to check the most significant bits
-    mask1 = 1 << 7
-    mask2 = 1 << 6
+    Args:
+        data: list of integers
 
-    for byte in data:
+    Returns:
+        True if valid UTF-8 encoding, otherwise False
+    """
+    bit_count = 0
+    for n in data:
         mask = 1 << 7
-        if num_bytes == 0:
-            # Count the number of leading 1's in the first byte
-            while mask & byte:
-                num_bytes += 1
+        if not bit_count:
+            while n & mask:
+                bit_count += 1
                 mask >>= 1
-
-            # 1 byte character
-            if num_bytes == 0:
-                continue
-
-            # Invalid scenarios according to UTF-8 encoding rules
-            if num_bytes == 1 or num_bytes > 4:
+            if bit_count > 4:
                 return False
-        else:
-            # Check that the byte starts with '10'
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-
-        num_bytes -= 1
-
-    return num_bytes == 0
+        elif n >> 6 != 2:
+            return False
+        if bit_count:
+            bit_count -= 1
+    return bit_count == 0

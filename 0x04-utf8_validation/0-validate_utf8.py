f687f653f6842af66012872d90e465e1d2427689
meron6 <khozameron5@gmail.com>
2024-08-25 08:34:55 -0700

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
+"""Module containing a function that checks if a list of ints is a valid
+utf8 character"""
+
 
 def validUTF8(data):
-    """Determines if the given list of integers represents valid UTF-8 encoding.
+    """[summary]
 
     Args:
-        data (list of int): List of integers representing bytes.
-
-    Returns:
-        bool: True if data represents valid UTF-8 encoding, otherwise False.
+        data ([type]): [description]
     """
-    bit_count = 0
-    
-    for byte in data:
-        if bit_count == 0:
-            # Determine how many leading 1 bits are in the current byte
-            if (byte >> 7) == 0b0:
-                # 1-byte character
-                bit_count = 0
-            elif (byte >> 5) == 0b110:
-                # 2-byte character
-                bit_count = 1
-            elif (byte >> 4) == 0b1110:
-                # 3-byte character
-                bit_count = 2
-            elif (byte >> 3) == 0b11110:
-                # 4-byte character
-                bit_count = 3
-            else:
-                # Invalid start byte
+    n_bytes = 0
+    for each in data:
+        bi_str = format(each, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bi_str:
+                if bit == '0':
+                    break
+                n_bytes += 1
+
+            if n_bytes == 0:
+                continue
+
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            # Check if the byte is a valid continuation byte (10xxxxxx)
-            if (byte >> 6) != 0b10:
+            if not (bi_str[0] == '1' and bi_str[1] == '0'):
                 return False
-            bit_count -= 1
+        n_bytes -= 1
+    return (n_bytes == 0)
 
-    # Ensure there are no pending bytes that are not properly closed
-    return bit_count == 0
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))  # True
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105, 115,
+    32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))  # True
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data)) # False
+
+    data = [235, 140]
+    print(validUTF8(data))  # False

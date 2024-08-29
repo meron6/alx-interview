af575b4b93008a33e69b223be2bd387b51d4478a
meron6 <khozameron5@gmail.com>
2024-08-29 10:45:34 -0700

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,41 +1,10 @@
 #!/usr/bin/python3
-"""N-Queens Problem Solver"""
+"""program that solves N queens puzzle"""
+import sys
 
 
-class NQueens:
-    """Solve the N-Queens problem."""
-
-    def __init__(self, n: int):
-        self.n = n
-        self.solutions = []
-        self.tracker = [-1] * n
-
-    def valid(self, col: int) -> bool:
-        """Check if placing a queen in the column is valid."""
-        for i in range(col):
-            if abs(self.tracker[i] - self.tracker[col]) in {0, col - i}:
-                return False
-        return True
-
-    def helper(self, index: int, cols):
-        """Recursive helper function to solve the problem."""
-        if index == self.n:
-            self.solutions.append(cols)
-            return
-        for i in range(self.n):
-            self.tracker[index] = i
-            if self.valid(index):
-                self.helper(index + 1, cols + [i])
-
-    def solve(self):
-        """Run the recursive solver and return all solutions."""
-        self.helper(0, [])
-        return [[(i, sol[i]) for i in range(self.n)] for sol in self.solutions]
-
-
-if __name__ == '__main__':
-    import sys
-
+def get_n():
+    """validates command line input"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -50,7 +19,83 @@ if __name__ == '__main__':
         print("N must be at least 4")
         sys.exit(1)
 
-    solver = NQueens(n)
-    solutions = solver.solve()
-    for res in solutions:
-        print(res)
+    return n
+
+
+def row_has_queen(board, row):
+    """check row for other queens"""
+    for has_queen in board[row]:
+        if has_queen:
+            return True
+    return False
+
+
+def col_has_queen(board, col):
+    """check col for other queens"""
+    n = len(board[0])
+    for row in range(n):
+        has_queen = board[row][col]
+        if has_queen:
+            return True
+    return False
+
+
+def diag_has_queen(board, row, col):
+    """check diagonals for queens"""
+    n = len(board[0])
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j]:
+            return True
+
+    for i, j in zip(range(row, -1, -1), range(col, n, 1)):
+        if board[i][j]:
+            return True
+
+    return False
+
+
+def print_board(board, n):
+    """prints the board"""
+    queens = [[i, j] for i in range(n) for j in range(n) if board[i][j]]
+    print(queens)
+
+
+def n_queens():
+    """make board"""
+    n = get_n()
+    board = [[False for j in range(n)] for i in range(n)]
+    solve_nqueens(0, 0, board)
+
+
+def solve_nqueens(idx, queens_found, board):
+    """recursive backtracking"""
+    n = len(board[0])
+    a = False
+
+    if idx >= n ** 2:
+        return False
+
+    i, j = [(j, i) for j in range(n) for i in range(n)][idx]
+
+    if not row_has_queen(board, i) and not col_has_queen(board, j) \
+       and not diag_has_queen(board, i, j):
+        b_true = [row[:] for row in board]
+        b_true[i][j] = True
+
+        if queens_found + 1 == n:
+            print_board(b_true, n)
+            return True
+
+        a = solve_nqueens(idx + 1, queens_found + 1, b_true)
+
+    b_false = [row[:] for row in board]
+    b_false[i][j] = False
+    b = solve_nqueens(idx + 1, queens_found, b_false)
+
+    if a or b:
+        return True
+
+
+if __name__ == "__main__":
+    n_queens()
